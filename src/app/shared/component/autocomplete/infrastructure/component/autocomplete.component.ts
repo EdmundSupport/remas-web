4930278714f2de9bb7d745f58de2263f9fd7c6b1
Edmundo Guerrero <edmundo.gt.esc@gmail.com>
2023-10-10 20:35:22 -0600

    perform autocomplete

--- src/app/shared/component/autocomplete/infrastructure/component/autocomplete.component.ts
+++ src/app/shared/component/autocomplete/infrastructure/component/autocomplete.component.ts
@@ -2,7 +2,7 @@ import { Component, EventEmitter, Input, Output, SimpleChanges } from "@angular/
 
 import { FormControl } from "@angular/forms";
 import { MatSnackBar } from "@angular/material/snack-bar";
-import { BehaviorSubject, Observable, catchError, finalize, map, startWith, switchMap } from "rxjs";
+import { BehaviorSubject, Observable, Subject, catchError, finalize, map, startWith, switchMap } from "rxjs";
 
 type OnDatasourceInterface<T> = (arg: string) => Observable<Array<T>> | Array<T>;
 
@@ -20,28 +20,18 @@ export class AutocompleteComponent<T>{
     @Output('onChange') onChange = new EventEmitter<string>();
     @Output('onSelected') onSelected = new EventEmitter<T>();
     @Input('onOptionShow') onOptionShow: (arg: T) => string = (() => '');
+    objSelected!: T | undefined;
     form = new FormControl();
     timer: any;
     constructor() {
-        console.log("🚀 ~ file: autocomplete.component.ts:25 ~ AutocompleteComponent<T> ~ constructor ~ constructor:")
         this.onLoadDatasource();
     }
 
     ngOnInit() {
-        console.log("🚀 ~ file: autocomplete.component.ts:29 ~ AutocompleteComponent<T> ~ ngOnInit ~ ngOnInit:")
-
-
-        console.log(this.placeholder, "🚀 ~ file: autocomplete.component.ts:31 ~ AutocompleteComponent<T> ~ ngOnInit ~ this.initial:", this.initial)
         this.form.setValue(this.onOptionShow(this.initial));
     }
 
-    ngOnChanges(changes: SimpleChanges) {
-        console.log("🚀 ~ file: autocomplete.component.ts:37 ~ AutocompleteComponent<T> ~ ngOnChanges ~ changes:", changes)
-        // if(!changes["initial"].previousValue && changes["initial"].currentValue) this.onLoadDatasource();
-    }
-
     onClickOption(obj: T) {
-        console.log(this.placeholder, "🚀 ~ file: autocomplete.component.ts:35 ~ AutocompleteComponent<T> ~ onClickOption ~ obj:", obj)
         this.onSelected.emit(obj);
     }
 
@@ -49,13 +39,11 @@ export class AutocompleteComponent<T>{
         this.form.valueChanges.pipe(
             startWith(this.onOptionShow(this.initial)),
             map((value) => {
-                console.log(this.placeholder, "🚀 ~ file: autocomplete.component.ts:43 ~ AutocompleteComponent<T> ~ map ~ value:", value)
                 return value;
             })).subscribe((value) => {
                 if (this.timer) clearTimeout(this.timer);
 
                 this.timer = setTimeout(() => {
-                    console.log(this.placeholder, "🚀 ~ file: autocomplete.component.ts:45 ~ AutocompleteComponent<T> ~ map ~ value:", value)
                     if (value) this.onChange.emit(value);
                 }, 500);
             });
