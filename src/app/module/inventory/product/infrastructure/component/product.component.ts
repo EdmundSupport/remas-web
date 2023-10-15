c4847e3f4a3b40617d572f7c782990de0bc94202
Edmundo Guerrero <edmundo.gt.esc@gmail.com>
2023-10-15 00:27:57 -0600

    working view products

--- src/app/module/inventory/product/infrastructure/component/product.component.ts
+++ src/app/module/inventory/product/infrastructure/component/product.component.ts
@@ -1,34 +1,105 @@
-import {
-    Component,
-} from '@angular/core';
-import { CalendarEvent, CalendarEventAction } from 'angular-calendar';
-import { parseISO } from 'date-fns';
-import { MatSnackBar } from '@angular/material/snack-bar';
-import { colors } from 'src/app/shared/color/domain/constant/color.constant';
-import { ProductService } from '../../application/service/product.service';
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
+import { MatSort, MatSortModule } from '@angular/material/sort';
+import { MatTableDataSource, MatTableDataSourcePaginator, MatTableModule } from '@angular/material/table';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { ProductInterface } from 'src/app/datasource/remas/domain/interface/product.interface';
-import { ActivatedRoute, Router } from '@angular/router';
-import { Store } from '@ngrx/store';
-import { increment } from 'src/app/shared/component/tool_bar/application/action/tool_bar.action';
-import { Observable } from 'rxjs';
+import { ProductService } from 'src/app/datasource/remas/application/service/inventory-product.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { BehaviorSubject, Subscription } from 'rxjs';
 
+type DisplayColumnsInterface = keyof ProductInterface | 'action';
 @Component({
     selector: 'app-product',
     templateUrl: '../page/product.page.html',
     styleUrls: ['../style/product.style.scss'],
 })
-export class ProductComponent {
-    events: CalendarEvent[] = [];
+export class ProductComponent implements AfterViewInit {
+    displayedColumns: DisplayColumnsInterface[] = ['sku', 'name', 'action'];
+    dataSource!: MatTableDataSource<ProductInterface>;
 
-    products: ProductInterface[] = [];
-    count$!: Observable<number>;
+    @ViewChild(MatPaginator) paginator!: MatPaginator;
+    @ViewChild(MatSort) sort!: MatSort;
+
+    onLoading$ = new BehaviorSubject<boolean>(false);
+
     constructor(
         private productService: ProductService,
         private matSnackBar: MatSnackBar,
-        private router: Router,
-        private route: ActivatedRoute,
-        private store: Store<{ count: number }>,
-    ) { 
-        this.count$ = store.select('count');
+    ) {
+        // Create 100 users
+        // const users = Array.from({length: 100}, (_, k) => createNewUser(k + 1));
+
+        // Assign the data to the data source for the table to render
+        // this.dataSource = new MatTableDataSource(users);
+    }
+
+    ngOnInit() {
+        this.onLoadProducts({
+            pagination: { limit: 5, offset: 0 }
+        });
+    }
+
+    ngAfterViewInit() {
+        if (this.dataSource?.paginator) this.dataSource.paginator = this.paginator;
+        if (this.dataSource?.sort) this.dataSource.sort = this.sort;
+    }
+
+    onEdit(product: ProductInterface) {
+
+    }
+
+    onDelete(product: ProductInterface) {
+
+    }
+
+    onLoad() {
+    }
+
+    onLoadProducts(filter: ProductInterface) {
+        return this.productService.onFind(filter).subscribe((result) => {
+            const newData: ProductInterface[] = [...this.dataSource?.data ?? [], ...result];
+            newData.map((item, index, array) => {
+                const arrayIndex = array.findIndex((arrayItem) => arrayItem.uuid == item.uuid);
+                if (arrayIndex == -1)
+                    array.splice(arrayIndex, 1);
+
+                return item;
+            });
+
+            this.dataSource = new MatTableDataSource(newData);
+        });
+    }
+
+    async applyFilter(event: Event) {
+        const filterValue = (event.target as HTMLInputElement).value;
+        const filter = filterValue.trim().toLowerCase();
+        if (this.dataSource) {
+            this.dataSource.filter = filter;
+
+            if (this.dataSource.filteredData.length == 0)
+                for (let index = 0; index < this.displayedColumns.length; index++) {
+                    const value = this.displayedColumns[index];
+                    if (this.dataSource.filteredData.length > 0) return;
+
+                    const payload: ProductInterface = { pagination: { offset: 0, limit: 5 } };
+                    if (value == 'sku') payload.sku = filter;
+                    if (value == 'name') payload.name = filter;
+
+                    if (value != 'action') {
+                        await (new Promise((resolve) => {
+                            this.onLoadProducts(payload).add(() => {
+                                this.dataSource.filter = filter;
+                                resolve(null);
+                            });
+                        }))
+                    }
+                }
+
+            if (this.dataSource.paginator) {
+                this.dataSource.paginator.firstPage();
+            }
+        }
     }
 }
--- src/app/module/inventory/product/product.module.ts
+++ src/app/module/inventory/product/product.module.ts
@@ -1,49 +1,37 @@
 // import { ProductFormComponent } from "./infrastructure/component/product-form.component";
-import { AutocompleteModule } from "src/app/shared/component/autocomplete/autocomplete.module";
-import { ButtonFloatComponent } from "src/app/shared/component/button-float/infrastructure/component/button-float.component";
-import { CalendarModule } from "src/app/shared/component/calendar/calendar.module";
 import { CommonModule } from "@angular/common";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
-import { MatAutocompleteModule } from '@angular/material/autocomplete';
-import { MatButtonModule } from '@angular/material/button';
-import { MatDatepickerModule } from '@angular/material/datepicker';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatIconModule } from "@angular/material/icon";
-import { MatInputModule } from "@angular/material/input";
-import { MatNativeDateModule } from '@angular/material/core';
-import { MatProgressSpinnerModule } from "@angular/material/progress-spinner";
 import { MatSnackBarModule } from '@angular/material/snack-bar';
-import { NgFor, AsyncPipe } from '@angular/common';
 import { NgModule } from "@angular/core";
 import { ProductComponent } from "./infrastructure/component/product.component";
 // import { ProductFormDetailComponent } from "./infrastructure/component/product-form-detail.component";
 import { ProductRoutingModule } from "./product-routing.module";
-import { SuperpositionLoadingComponent } from "src/app/shared/component/superposition-loading/infrastructure/component/superposition-loading.component";
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+import { MatTableModule } from '@angular/material/table';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from "@angular/material/icon";
+import {MatButtonModule} from '@angular/material/button';
 @NgModule({
     declarations: [
         ProductComponent,
         // ProductFormComponent,
         // ProductFormDetailComponent,
     ],
-    imports: [        
-        AutocompleteModule,
-        ButtonFloatComponent,
-        CalendarModule,
+    imports: [
         CommonModule,
         FormsModule,
-        MatAutocompleteModule,
-        MatButtonModule,
-        MatDatepickerModule,
-        MatFormFieldModule,
-        MatIconModule,
-        MatInputModule,
-        MatNativeDateModule,
-        MatProgressSpinnerModule,
         MatSnackBarModule,
         ProductRoutingModule,
+        MatPaginatorModule,
+        MatSortModule,
+        MatTableModule,
+        MatInputModule,
+        MatFormFieldModule,
+        MatIconModule,
         ReactiveFormsModule,
-        SuperpositionLoadingComponent,
-
+        MatButtonModule
     ]
 })
 export class ProductModule { }
